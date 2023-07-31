9818e78b74fc581bf2a231bec1d06e10690275df
Aman Agarwal <agarwalaman190202@gmail.com>
2023-07-31 09:39:30 +0530

    Added flags

--- good_boy.py
+++ good_boy.py
@@ -2,7 +2,17 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("Name")
+parser.add_argument("-b", "--boy", action="store_true")
+parser.add_argument("-g", "--girl", action="store_true")
 
 args = parser.parse_args()
 
-print(f"{args.Name} is a good man")
+if(args.boy):
+    print(f"{args.Name} is a good man")
+
+elif(args.girl):
+    print(f"{args.Name} is a good girl")
+
+else:
+    print(f"{args.Name} is a good person")
+
